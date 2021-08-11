d44bfd9190298fae5189043429c33d30ded8fc4f
Pranav <prn2108@gmail.com>
2021-08-11 22:12:59 +0530

    feat: added drawer to navbar

--- client/src/components/Navigation/MainNavigation.js
+++ client/src/components/Navigation/MainNavigation.js
@@ -1,22 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import MainHeader from './MainHeader';
+import SideDrawer from './SideDrawer';
+import BackDrop from '../UIElements/BackDrop';
 import NavLinks from './NavLinks';
 import logo from '../../assets/logo.png';
 import './MainNavigation.css';
 
 const MainNavigation = (props) => {
+
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    }
+
+    const closeDrawer = () => {
+        setDrawerIsOpen(false);
+    }
+
     return (
         <React.Fragment>
+            {
+                drawerIsOpen && <BackDrop onClick={closeDrawer} />
+            }
+            <SideDrawer onClick={closeDrawer} show={drawerIsOpen} >
+                    <nav className="main-navigation__drawer-nav">
+                        <NavLinks />
+                    </nav>
+                </SideDrawer>
+            
             <MainHeader>
-                <button className="main-navigation__menu-btn">
+                <button onClick={openDrawer} className="main-navigation__menu-btn">
                     <span />
                     <span />
                     <span />
                 </button>
-                <img style={{marginBottom:"7px"}} src={logo} className="main-navigation__title" />
+                <img style={{ marginBottom: "7px" }} src={logo} className="main-navigation__title" />
                 <nav className="main-navigation__header-nav">
-                   <NavLinks />
+                    <NavLinks />
                 </nav>
             </MainHeader>
         </React.Fragment>
