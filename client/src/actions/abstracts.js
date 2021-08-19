34a7ed6f1f3f1ef3cfd2d2e9b403bd63e758261f
Pranav <prn2108@gmail.com>
2021-08-19 23:30:30 +0530

    feat: added action constants

--- client/src/actions/abstracts.js
+++ client/src/actions/abstracts.js
@@ -1,9 +1,10 @@
+import { FETCH_ALL, CREATE, DELETE } from '../constants/actionTypes';
 import * as api from '../api';
 
 export const getAbstracts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchAbstracts();
-        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
         console.log(error);
     }
@@ -13,7 +14,7 @@ export const getAbstracts = () => async (dispatch) => {
 export const createAbstract = (abstract) => async (dispatch) => {
     try {
       const { data } = await api.createAbstract(abstract);
-      dispatch({ type: 'CREATE', payload: data });
+      dispatch({ type: CREATE, payload: data });
     } catch (error) {
       console.log(error);
     }
@@ -23,7 +24,7 @@ export const deleteAbstract = (id) => async (dispatch) => {
   try {
     
     await api.deleteAbstract(id);
-    dispatch({ type: 'DELETE', payload: id });
+    dispatch({ type: DELETE, payload: id });
     console.log('delete dispatched');
   } catch (error) {
     console.log(error);
